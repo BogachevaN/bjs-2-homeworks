6b34b3f8fcb49c3619370767ed7ba236e0ee82b7
Natali Bogacheva <npbogacheva@gmail.com>
2024-01-18 22:47:30 +0300

    add solveEquation function

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,7 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = Math.pow(b, 2)
+  let d = Math.pow(b, 2) - 4*a*c
   if (d === 0) {
     arr.push(-b/(2*a))
   } else if (d > 0) {
