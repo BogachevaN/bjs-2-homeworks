85cd88430b551ce8adc589ffaad21831360896e5
Natali Bogacheva <npbogacheva@gmail.com>
2024-01-20 18:52:16 +0300

    fix codestyle

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,23 +1,24 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  let d = Math.pow(b, 2) - 4*a*c
-  if (d === 0) {
-    arr.push(-b/(2*a))
-  } else if (d > 0) {
-    arr.push((-b + Math.sqrt(d) )/(2*a))
-    arr.push((-b - Math.sqrt(d) )/(2*a))
-  }
-  return arr;
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c
+	if (d === 0) {
+		arr.push(-b / (2 * a))
+	} else if (d > 0) {
+		arr.push((-b + Math.sqrt(d)) / (2 * a))
+		arr.push((-b - Math.sqrt(d)) / (2 * a))
+	}
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percentInMonth = (percent/100)/12
-  let creditBody = amount - contribution
-  let payment = creditBody * (percentInMonth + (percentInMonth / ((Math.pow(1 + percentInMonth, countMonths)) - 1)))
-  let total = 0
-  for (let i = 0; i < countMonths; i++) {
-    total += payment
-  }
-  return Number(total.toFixed(2))
+	let percentInMonth = (percent / 100) / 12
+	let creditBody = amount - contribution
+	let payment = creditBody * (percentInMonth + (percentInMonth / ((Math.pow(1 + percentInMonth, countMonths)) - 1)))
+	let total = 0
+	for (let i = 0; i < countMonths; i++) {
+		total += payment
+	}
+	return Number(total.toFixed(2))
 }
