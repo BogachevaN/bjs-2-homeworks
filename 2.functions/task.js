056b1fc08c86848cc181ccd7a42d1f9ec48a3a39
Natali Bogacheva <npbogacheva@gmail.com>
2024-01-24 21:55:43 +0300

    add getArrayParams

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,11 @@
 function getArrayParams(...arr) {
   let min = Math.min.apply(null, arr)
   let max = Math.max.apply(null, arr)
-  let sum = arr.reduce.apply(null,arr)
+  let initialValue = 0
+  let sum = arr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+  )
   let avg = sum / arr.length
   return { min: min, max: max, avg: avg };
 }
