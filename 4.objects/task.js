7b0f302a2e3f3760e77e5919ebd3cd1a9e3c80c5
Natali Bogacheva <npbogacheva@gmail.com>
2024-01-29 13:47:19 +0300

    add Student object

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name
+  this.gender = gender
+  this.age = age
+  this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if(this?.marks) {
+        this.marks = [...marks]
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(this?.marks || this.marks.length === 0) {
+        let initialValue = 0
+        let sum = this.marks.reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            initialValue,
+          )
+          return sum / this.marks.length
+    } else {
+        return null
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject
+    delete this.marks
+    this.excluded = reason
 }
