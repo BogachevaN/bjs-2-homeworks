a7e9cdb324d90a6d930da47c11a22fa1d491b814
Natali Bogacheva <npbogacheva@gmail.com>
2024-02-07 14:25:44 +0300

    add task1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,16 @@
-﻿
+﻿function parseCount(value) {
+    let result = Number.parseFloat(value)
+    if (result !== NaN) {
+        return result
+    } else {
+        throw new Error("Невалидное значение")
+    }
+}
+
+function validateCount(value) {
+    try {
+        parseCount(value)
+      } catch (error) {
+        return error
+      }
+}
