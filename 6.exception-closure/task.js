a5f5df9225afab9457c6c307727875f5e0485419
Natali Bogacheva <npbogacheva@gmail.com>
2024-02-07 16:36:30 +0300

    add task2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -14,3 +14,36 @@ function validateCount(value) {
         return error
     }
 }
+
+class Triangle {
+    constructor(a, b, c) {
+        if ( a + b < c || a + c < b || c + b < a) {
+            this.a = a
+            this.b = b
+            this.c = c
+        } else {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }    
+    }
+
+    perimeter() {
+        try {
+            return Number(this.a + this.b + this.c) 
+        } catch (error) {
+            return  'Ошибка! Треугольник не существует'
+        }
+    }
+
+    area() {
+        let p = this.perimeter / 2
+        return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(2))
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c)
+    } catch {
+        this.perimeter
+    }
+}
