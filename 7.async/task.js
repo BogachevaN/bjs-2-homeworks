8c4772f6cc0d0495672a6877331387ca933b7f4d
Natali Bogacheva <npbogacheva@gmail.com>
2024-02-13 19:18:29 +0300

    add AlarmClock

---
+++ 7.async/task.js
@@ -0,0 +1,61 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = []
+        this.intervalId = null
+    }
+
+    addClock(time, callback) {
+        if (time && callback) {
+            let alarm = this.alarmCollection.filter((alarm) => alarm.time === time)
+            if (alarm) {
+                console.warn('Уже присутствует звонок на это же время')
+            }
+        } else {
+            throw new Error('Отсутствуют обязательные аргументы')
+        }
+
+        let callbackResulat = callback()
+        this.alarmCollection.push({
+            callback: callback,
+            time: time,
+            canCall: callbackResulat
+        })
+    }
+
+    removeClock(time) {
+        let clearArray = this.alarmCollection.filter((alarm) => alarm.time !== time)
+        this.alarmCollection = clearArray
+    }
+
+    getCurrentFormattedTime() {
+        const date = new Date()
+        return date.getHours() + ":" + date.getMinutes()
+    }
+
+    start() {
+        if (!this.intervalId) {
+            this.intervalId = setInterval(() => {
+                this.alarmCollection.forEach((alarm) => {
+                    if (alarm.time === getCurrentFormattedTime() && alarm.canCall === true) {
+                        alarm.canCall = false
+                        alarm.callback()
+                    }
+                })
+            }, 1000)
+        }
+    }
+
+    stop() {
+        clearInterval(this.intervalId)
+        this.intervalId = null
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach((alarm) => alarm.canCall = true)
+    }
+
+    clearAlarms() {
+        this.stop()
+        this.alarmCollection = []
+    }
+}
